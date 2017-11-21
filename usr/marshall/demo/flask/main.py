7622e50e21d5f0d3b42afb92dfc0463af79f9f72
Marshall Clark <gmarshallclark@gmail.com>
2017-11-21 17:08:30 -0500

    adding quickreact demo app

--- usr/marshall/demo/flask/main.py
+++ usr/marshall/demo/flask/main.py
@@ -6,7 +6,11 @@ import dbcalls
 #route for hello world
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return( 
+    	"<p>Hello flask!</p>"\
+    	"<a href='http://localhost:5000/gaz'>Try Gaz at localhost:5000/gaz</a><br />"\
+    	"<a href='http://localhost:5000/gaz/37'>Try Gaz for NC at localhost:5000/gaz/37</a><br />"
+    	)
 
 #two route definitions going to one method
 # # one route with param and one without
@@ -14,4 +18,6 @@ def hello():
 @app.route('/gaz/<int:state_fips>', methods=['GET'])
 def startup(state_fips=None):
     gazdat = dbcalls.gaz(state_fips)
-    return jsonify( gazdat )
+    response = jsonify(gazdat);
+    response.headers.add('Access-Control-Allow-Origin','*');
+    return response
