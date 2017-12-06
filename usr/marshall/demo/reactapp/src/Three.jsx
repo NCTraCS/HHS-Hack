21344f160d7c455029be566f4ef68f2edd9f00f6
Marshall Clark <gmarshallclark@gmail.com>
2017-12-06 12:46:09 -0500

    scatter demo chart

--- usr/marshall/demo/reactapp/src/Three.jsx
+++ usr/marshall/demo/reactapp/src/Three.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {VictoryChart, VictoryScatter, VictoryTheme} from 'victory';
 
 class Three extends Component {
 	constructor() {
@@ -6,7 +7,26 @@ class Three extends Component {
 	}
 	render() {
 		return(
-      		<h1>This is component Three</h1>
+			<div>
+      			<h1>victory scatter</h1>
+      			<VictoryChart
+      				height={200}
+  					theme={VictoryTheme.material}
+  					domain={{ x: [0, 5], y: [0, 7] }}
+					>
+  					<VictoryScatter
+    					style={{ data: { fill: "#aaaaaa" } }}
+    					size={5}
+    					data={[
+      					{ x: 1, y: 2 },
+      					{ x: 2, y: 3 },
+      					{ x: 3, y: 5, size: 9, fill: "#c43a31" },
+      					{ x: 4, y: 4 },
+      					{ x: 5, y: 7 }
+    					]}
+  					/>
+					</VictoryChart>
+			</div>
       );
 	}
 }
