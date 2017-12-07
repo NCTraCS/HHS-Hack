b812fdc62b150c2764149b11d056eec131a17722
Robert Bradford <rbrad@med.unc.edu>
2017-12-07 16:11:45 -0500

    Version 0.2.0!!!!!!! FUCK YEA!

--- flask/coOccur.py
+++ flask/coOccur.py
@@ -18,6 +18,23 @@ def coDxList(op_dx_limit=100,co_dx_limit=100):
 		res['co_dx_name'] = parts[1]
 	return result
 
+
+def coPcdList(op_pcd_limit=100,co_pcd_limit=100):
+	conn = dbconnect.connect()
+
+	sql = "select * , procdesc as display_name from co_pcd_options"
+
+	cursor = conn.cursor()
+	#cursor.execute(sql,(op_pcd_limit,co_pcd_limit))
+	cursor.execute(sql)
+
+	result = cursor.fetchall()
+	for res in result:
+		parts = res['PROCDESC'].split(':')
+		res['co_pcd_code'] = parts[0]
+		res['co_pcd_name'] = parts[1]
+	return result
+
 def test():
 	result = coDxList(200,200)
 	for res in result:
